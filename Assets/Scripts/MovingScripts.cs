609e63c98eed55957154ccc664fafb059bb9e36a
jaideedavi <jaidee.davi@bumail.net>
2026-03-05 17:41:08 +0700

    Moving Code

--- Assets/Scripts/MovingScripts.cs
+++ Assets/Scripts/MovingScripts.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class MovingScripts : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+     public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveX, 0f, moveZ).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (movement != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+        }
     }
 }
